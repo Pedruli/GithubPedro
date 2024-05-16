57c1bcfebb2373358bb267dd0dd7435c3cde0d78
Pedro <pedrofuglini@gmail.com>
2024-05-16 00:36:44 -0300

    agregar tablas

---
+++ practica.script.js
@@ -0,0 +1,9 @@
+var nombre = "Juan";
+var apellido = "Pérez";
+console.log(nombre + apellido);
+var num1 = 10;
+var num2 = 5;
+var suma = num1 + num2;
+console.log(suma);
+var nuevaSuma = suma + 3;
+console.log(nuevaSuma);
